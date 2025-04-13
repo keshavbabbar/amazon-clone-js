f5ec4148bfeb20395dc1336d3c3f39a25e12965f
Keshav Babbar <keshavbabbar007@gmail.com>
2025-04-13 10:51:58 +0530

    update

--- amazon.js
+++ amazon.js
@@ -1,38 +1,3 @@
-const products = [ 
-    {
-        image: 'images/products/athletic-cotton-socks-6-pairs.jpg',
-        name: 'Black and Gray Athletic Cotton Socks - 6 Pairs',
-        rating: 
-        {
-          stars: 4.5,
-          count: 87 
-        },
-        price: 1090
-    },
-
-    {
-        image: 'images/products/intermediate-composite-basketball.jpg',
-        name: 'Intermediate Size Basketball',  
-        rating: 
-        {
-          stars: 4,
-          count: 127
-        }  ,
-        price: 2095    
-    },
-
-    {
-        image: 'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-        name: 'Adults Plain Cotton T-Shirt - 2 Pack',
-        rating: 
-        {
-          stars: 4.5,
-          count: 56
-        },
-        price: 799    
-    }
-]
-
   let productsHTML = '';
 
   products.forEach((product) => {
@@ -40,23 +5,23 @@ const products = [
        <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
-              src="${products.image}">
+              src="${product.image}">
           </div>
 
           <div class="product-name limit-text-to-2-lines">
-           ${products.name}
+           ${product.name}
           </div>
 
           <div class="product-rating-container">
             <img class="product-rating-stars"
-              src="images/ratings/rating-${products.rating.stars}.png">
+              src="images/ratings/rating-${(product.rating.stars) * 10}.png">
             <div class="product-rating-count link-primary">
-            ${products.rating.count}
+            ${product.rating.count}
             </div>
           </div>
 
           <div class="product-price">
-            $${products.price}
+            $${(product.priceCents / 100).toFixed(2)}
           </div>
 
           <div class="product-quantity-container">
