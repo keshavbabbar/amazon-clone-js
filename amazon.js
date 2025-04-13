c2d9b8b6eaddb856fd0ba146c780df0318dc799c
Keshav Babbar <keshavbabbar007@gmail.com>
2025-04-13 12:27:42 +0530

    update

--- amazon.js
+++ amazon.js
@@ -46,10 +46,23 @@
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button js-add-to-cart button-primary" 
+          data-product-name = "${product.name}">
             Add to Cart
           </button>
         </div> `
   })
 
-  document.querySelector('.js-products-grid').innerHTML = productsHTML;
+    document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+    document.querySelectorAll('.js-add-to-cart').
+    forEach((button) => {
+      button.addEventListener('click', () => {
+         const productName = button.dataset.productName;
+         cart.push({
+          productName: productName,
+          quantity: 1
+         });
+         console.log(cart)
+      })
+    })
