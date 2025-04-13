d88ddd7f85a3adf6fcd3c3fd1f48211ad8f0a5fd
Keshav Babbar <keshavbabbar007@gmail.com>
2025-04-13 14:40:45 +0530

    update

--- amazon.js
+++ amazon.js
@@ -1,7 +1,7 @@
-  let productsHTML = '';
+let productsHTML = '';
 
-  products.forEach((product) => {
-    productsHTML += `
+products.forEach((product) => {
+  productsHTML += `
        <div class="product-container">
           <div class="product-image-container">
             <img class="product-image"
@@ -51,35 +51,37 @@
             Add to Cart
           </button>
         </div> `
-  })
+})
 
-    document.querySelector('.js-products-grid').innerHTML = productsHTML;
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-    document.querySelectorAll('.js-add-to-cart').forEach((button) => {
-      button.addEventListener('click', () => 
-      {
-         const productId = button.dataset.productId;
-         let matchingItem ;
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    const productId = button.dataset.productId;
+    let matchingItem;
 
-         cart.forEach((item) => 
-          {
-              if (productId === item.productId) 
-              {
-                matchingItem =item;
-              }
-          });
-              if (matchingItem)
-                {
-                 matchingItem.quantity += 1;
-                }
-          
-             else
-             {
-               cart.push({
-                    productId: productId,
-                    quantity: 1
-                  })
-             }
-       console.log(cart)
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    }
+
+    else {
+      cart.push({
+        productId: productId,
+        quantity: 1
       })
+    }
+
+    let cartQuantity = 0;
+    cart.forEach((item) => {
+      cartQuantity += item.quantity;
     })
+
+    document.querySelector('.js-cart-quantity').innerHTML = cartQuantity
+
+  })
+})
