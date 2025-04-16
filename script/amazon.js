ff2cda7a3539e81ca18509c32b62a8db68318ce6
Keshav Babbar <keshavbabbar007@gmail.com>
2025-04-16 15:13:37 +0530

    update

--- script/amazon.js
+++ script/amazon.js
@@ -1,6 +1,8 @@
 import { cart, addToCart, calculateCartQuantity } from '../data/cart.js'
 import { products } from '../data/products.js'
 import { formatCurrency } from './utils/money.js';
+
+
 let productsHTML = '';
 
 products.forEach((product) => {
