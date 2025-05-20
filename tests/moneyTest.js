64482579198343d6f21fc083d19cdfc573cfae20
Keshav Babbar <keshavbabbar007@gmail.com>
2025-05-20 18:38:35 +0530

    update

---
+++ tests/moneyTest.js
@@ -0,0 +1,26 @@
+import { formatCurrency } from "../script/utils/money.js";
+console.log('convert cents into dollars')
+if(formatCurrency(2095) === '20.95'){
+    console.log("passed");
+}
+
+else{
+    console.log('failed');
+}
+
+console.log('working with zero')
+if (formatCurrency(0) === '0.00'){
+    console.log('passed')
+}
+else{
+    console.log('faild')
+}
+
+
+console.log('round up the nearest cent')
+if (formatCurrency(2000.5) === '20.01'){
+    console.log('passed')
+}
+else{
+    console.log('failed')
+}
